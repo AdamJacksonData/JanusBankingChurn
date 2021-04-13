3e4ed323a18ac6464250053571845a4645f2e38b
Connor <connorcolman@kubrickgroup.com>
2021-04-13 11:49:54 +0100

    more plot updates

--- bc_exploration.py
+++ bc_exploration.py
@@ -19,10 +19,10 @@ customers_file_path = os.getcwd() + r'/customers_tm1_e.csv'
 transactions_file_path = os.getcwd() + r'/transactions_tm1_e.csv'
 customer_raw = pd.read_csv(customers_file_path)
 transaction_raw = pd.read_csv(transactions_file_path)
+
+
 # %%
-
 customer_raw['state'].replace(to_replace={'NY':'New York','TX':'Texas','CALIFORNIA':'California','UNK':'Nebraska','MASS':'Massachusetts', 'District of Columbia':'Washington'},inplace=True)
-
 combined_df = pd.merge(customer_raw, transaction_raw, on='customer_id', how='left')
 
 # dtype to date
@@ -30,13 +30,13 @@ for col in ['transaction_date', 'creation_date', 'dob', 'date']:
     combined_df[col] = pd.to_datetime(combined_df[col])#.dt.strftime('%Y-%m-%d')
 
 # %%
-
 combined_df.drop(combined_df[combined_df['state']=='Australia'].index,inplace=True)
 combined_df.drop(combined_df[combined_df['state']=='-999'].index,inplace=True)
 
 combined_df.sort_values(by=['customer_id', 'transaction_date'], inplace=True, ascending=True)
-# %%
 
+
+# %%
 # =============================================================================
 # Most recent transactions
 # =============================================================================
@@ -44,16 +44,9 @@ combined_df['last_transaction'] = ~combined_df.duplicated(subset=['customer_id']
 combined_df.sort_values(by=['customer_id', 'transaction_date'], inplace=True)
 
 
-# %%
-
-# =============================================================================
-# Running account balance for each customer
-# =============================================================================
-combined_df['running_account_total'] = combined_df.groupby(['customer_id'])['amount'].cumsum() + combined_df['start_balance']
 
 
 # %%
-
 neg_sb_df = combined_df[combined_df['start_balance']<0]
 large_sb_df = combined_df[combined_df['start_balance']>2.5e8]
 
@@ -69,10 +62,14 @@ high_amount = combined_df[combined_df['amount']>2.5e8]
 combined_df.drop(combined_df[combined_df['amount']>2.5e8].index, inplace=True)
 low_amount = combined_df[combined_df['amount']<-1e8]
 combined_df.drop(combined_df[combined_df['amount']<-1e8].index, inplace=True)
-#low_amount = combined_df[combined_df['running_account_total']<-1e8]
-#high_amount = combined_df[combined_df['running_account_total']>2.5e8]
-#combined_df.drop(combined_df[(combined_df['running_account_total']>2.5e8) | (combined_df['running_account_total']<-1e8)].index, inplace=True)
 df_stats = combined_df.describe()
+
+
+# %%
+# =============================================================================
+# Running account balance for each customer
+# =============================================================================
+combined_df['running_account_total'] = combined_df.groupby(['customer_id'])['amount'].cumsum() + combined_df['start_balance']
 
 
 # %%
@@ -91,7 +88,7 @@ pd.plotting.scatter_matrix(df_sample.drop(columns='last_transaction'), c=df_samp
 # =============================================================================
 customer_raw.drop(customer_raw[abs(customer_raw['start_balance'])>1e7].index, inplace=True)
 
-temp_df = customer_raw[['new_accounts_open', 'creation_date']]
+temp_df = customer_raw[['start_balance', 'creation_date']]
 temp_df['creation_date'] = pd.to_datetime(temp_df['creation_date'])
 temp_df = temp_df.set_index('creation_date')
 temp_df_m = temp_df.resample('M').sum()
@@ -102,5 +99,9 @@ port_df_m = port_df[['amount']].resample('M').sum()
 
 portfolio_df = pd.merge(temp_df_m, port_df_m, left_index=True, right_index=True)
 portfolio_df.drop(index=pd.to_datetime('2020-05-31 00:00:00'), inplace=True)
-portfolio_df['total_monthly_change'] = portfolio_df['new_accounts_open' + portfolio_df['amount']
+portfolio_df['total_monthly_change'] = portfolio_df['start_balance'] + portfolio_df['amount']
 portfolio_df['portfolio_balance']    = portfolio_df['total_monthly_change'].cumsum()
+portfolio_df['zeros'] = np.zeros((160,1))
+
+
+portfolio_df[['portfolio_balance', 'start_balance', 'amount', 'total_monthly_change', 'zeros']].plot(xlabel='Date', ylabel='Portfolio balence in USD')
