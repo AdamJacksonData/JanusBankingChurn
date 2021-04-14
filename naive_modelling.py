dfdd0d3b91999fdcebad1b2dc680c00fef060581
Adam <adamjackson@kubrickgroup.com>
2021-04-14 12:48:00 +0100

    Committing changes to naive model

--- naive_modelling.py
+++ naive_modelling.py
@@ -16,7 +16,10 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import plot_tree
-
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import roc_curve, roc_auc_score, plot_roc_curve, plot_precision_recall_curve
+from sklearn.calibration import calibration_curve
 
 # %%
 
@@ -31,8 +34,14 @@ raw_data['Age'] = raw_data['Age'].apply(np.floor)
 
 preprocessed_df = raw_data.drop(columns=['customer_id', 'deposit', 'withdrawal', 'dob', 'creation_date', 'account_id', 'end_date'])
 #preprocessed_df = preprocessed_df.drop(columns='transaction_date')
-preprocessed_df = preprocessed_df.drop(columns='state')
 
+dummies = pd.get_dummies(preprocessed_df['state'], drop_first=False)
+states = sorted(preprocessed_df['state'].unique())
+
+for i in range(len(states)):
+    preprocessed_df[states[i]] = dummies[dummies.columns[i]]
+
+preprocessed_df = preprocessed_df.drop(columns='state')
 # %%
 
 test_data = preprocessed_df[preprocessed_df['transaction_date']=='2020-05']
@@ -42,9 +51,13 @@ test_data.drop(columns=['transaction_date','last_transaction'],inplace=True)
 train_val_data['churn'] = train_val_data['last_transaction'].map({True:1,False:0})
 train_val_data.drop(columns=['transaction_date','last_transaction'],inplace=True)
 
+churners = train_val_data[train_val_data['churn']==1]
+matched_non_churners = train_val_data[train_val_data['churn']==0].sample(98764, random_state=123)
+
+train_val_data_matched = churners.append(matched_non_churners)
 # %%
 
-train_data, val_data = train_test_split(train_val_data, test_size=0.2, stratify=train_val_data['churn'], shuffle=True, random_state=123)
+train_data, val_data = train_test_split(train_val_data_matched, test_size=0.2, stratify=train_val_data['churn'], shuffle=True, random_state=123)
 
 X_train = train_data.drop(columns='churn')
 y_train = train_data['churn']
@@ -58,20 +71,64 @@ X_train = pd.DataFrame(scaler.fit_transform(X_train),columns=X_train.columns)
 X_val = pd.DataFrame(scaler.transform(X_val),columns=X_val.columns)
 
 # %%
-
         
-md = np.random.randint(1, 10)
-msl = np.random.randint(1, 25)
-model = GradientBoostingClassifier(learning_rate=lr, max_depth=md, min_samples_leaf=msl)
+#md = np.random.randint(1, 10)
+#msl = np.random.randint(1, 25)
+#model = GradientBoostingClassifier(learning_rate=lr, max_depth=md, min_samples_leaf=msl)
+model = GradientBoostingClassifier()
 
 model.fit(X_train, y_train)
 y_hat_train = model.predict(X_train)
 train_acc = accuracy_score(y_train, y_hat_train)
 
 y_hat_val = model.predict(X_val)
+y_hat_val_proba = model.predict_proba(X_val)
 val_acc = accuracy_score(y_val, y_hat_val)
 
 feature_importances = model.feature_importances_
+
+# %%
+
+# =============================================================================
+# Plotting ROC curve
+# =============================================================================
+
+confusion = confusion_matrix(y_val, y_hat_val)
+TPR = confusion[1][1]/(confusion[1][1]+confusion[1][0])
+TNR = confusion[0][0]/(confusion[0][0]+confusion[0][1])
+
+fpr, tpr, thesholds = roc_curve(y_val, y_hat_val_proba[:,1])
+auc = roc_auc_score(y_val, y_hat_val_proba[:,1])
+plot_roc_curve(model, X_val, y_val)
+
+
+# %%
+# =============================================================================
+# Plotting PR curve
+# =============================================================================
+
+plot_precision_recall_curve(model, X_val, y_val)
+
+# %%
+# =============================================================================
+# Plotting calibration curve
+# =============================================================================
+
+fraction_of_positives, mean_predicted_value = calibration_curve(y_val, y_hat_val_proba[:,1], n_bins=10)
+
+x_perf = np.linspace(0.05,0.9,10)
+y_perf = np.linspace(0.05,0.9,10)
+fig, ax = plt.subplots()
+ax.plot(mean_predicted_value, fraction_of_positives, marker='o')
+ax.plot(x_perf, y_perf, c='red', linestyle='--')
+ax.set_xlabel('Predictions')
+ax.set_ylabel('Proportion of positive observations')
+ax.grid()
+ax.set_xlim(0,1)
+ax.set_ylim(0,1)
+ax.set_title('Calibration Curve')
+
+
 # %%
 
 
